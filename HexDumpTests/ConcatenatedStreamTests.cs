1511542e65e9446ef3a2ef8410d2993099329067
Edward Thomson <ethomson@edwardthomson.com>
2018-09-13 16:48:34 -0700

    Tests!

---
+++ HexDumpTests/ConcatenatedStreamTests.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using Xunit;
+using HexDump;
+
+namespace HexDumpTests
+{
+    public class ConcatenatedFileStreamTests
+    {
+        [Fact]
+        public void CanCatenateSingleStream()
+        {
+            var buffer = new byte[5];
+
+            var one = new MemoryStream(new byte[] { 1, 2, 3, 4, 5 });
+            var concatenated = new ConcatenatedStream(new Stream[] { one });
+
+            concatenated.Read(buffer, 0, 5);
+
+            Assert.Equal(new byte[] { 1, 2, 3, 4, 5 }, buffer);
+        }
+
+        [Fact]
+        public void CanConcatenateTwoStreams()
+        {
+            byte[] buffer = new byte[10];
+
+            var one = new MemoryStream(new byte[] { 1, 2, 3, 4, 5 });
+            var two = new MemoryStream(new byte[] { 6, 7, 8, 9, 10 });
+            var concatenated = new ConcatenatedStream(new Stream[] { one, two });
+
+            concatenated.Read(buffer, 0, 10);
+
+            Assert.Equal(new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, buffer);
+        }
+
+        [Fact]
+        public void CanConcatenateThreeStreams()
+        {
+            byte[] buffer = new byte[15];
+
+            var one = new MemoryStream(new byte[] { 1, 2, 3, 4, 5 });
+            var two = new MemoryStream(new byte[] { 6, 7, 8, 9, 10 });
+            var three = new MemoryStream(new byte[] { 11, 12, 13, 14, 15 });
+            var concatenated = new ConcatenatedStream(new Stream[] { one, two, three });
+
+            concatenated.Read(buffer, 0, 15);
+
+            Assert.Equal(new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }, buffer);
+        }
+    }
+}
---
+++ HexDumpTests/ForwardSeekingStreamTest.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using Xunit;
+using HexDump;
+
+namespace HexDumpTests
+{
+    public class ForwardSeekingStreamTest
+    {
+        [Fact]
+        public void CanFastForwardStreamFromStart()
+        {
+            var buffer = new byte[3];
+
+            var stream = new MemoryStream(new byte[] { 1, 2, 3, 4, 5 });
+            var forwardable = new ForwardSeekingStream(stream);
+
+            forwardable.Seek(2, SeekOrigin.Begin);
+
+            forwardable.Read(buffer, 0, 3);
+
+            Assert.Equal(new byte[] { 3, 4, 5 }, buffer);
+        }
+
+        [Fact]
+        public void CanFastForwardStreamAfterRead()
+        {
+            var buffer = new byte[2];
+
+            var stream = new MemoryStream(new byte[] { 1, 2, 3, 4, 5 });
+            var forwardable = new ForwardSeekingStream(stream);
+
+            forwardable.Read(buffer, 0, 2);
+            Assert.Equal(new byte[] { 1, 2 }, buffer);
+
+            forwardable.Seek(1, SeekOrigin.Current);
+
+            forwardable.Read(buffer, 0, 2);
+            Assert.Equal(new byte[] { 4, 5 }, buffer);
+        }
+    }
+}
